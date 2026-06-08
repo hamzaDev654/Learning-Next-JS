d0f71736d8d869aa00be78b92fce4a4dfa3c565e
hamzaDev654 <imhamza67@gmail.com>
2026-06-08 22:40:38 +0530

    changes

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -3,16 +3,19 @@ import Link from "next/link";
 import style from "@/style/meals.module.css";
 import { getMeals } from "../../../lib/meals";
 import MealsGrid from "@/components/meals/MealsGrid";
-
+// here
 const MealsPage: FC = async () => {
   const meals = await getMeals();
   return (
     <>
       <header className={style.header}>
         <h1>
-          Delicious meals, created <span className={style.highlight}>by you</span>
+          Delicious meals, created{" "}
+          <span className={style.highlight}>by you</span>
         </h1>
-        <p>Choose your favorite recipe and cook it yourself . It is easy and fun!</p>
+        <p>
+          Choose your favorite recipe and cook it yourself . It is easy and fun!
+        </p>
         <p className={style.cta}>
           <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { FC, ReactNode } from "react";
 import style from "@/style/navlink.module.css";
 import { usePathname } from "next/navigation";
-
+// jerer
 interface props {
   href: string;
   children: ReactNode;
@@ -12,7 +12,12 @@ interface props {
 const NavLink: FC<props> = ({ href, children }) => {
   const path = usePathname();
   return (
-    <Link href={href} className={path.startsWith(href) ? `${style.link} ${style.active}` : style.link}>
+    <Link
+      href={href}
+      className={
+        path.startsWith(href) ? `${style.link} ${style.active}` : style.link
+      }
+    >
       {children}
     </Link>
   );
