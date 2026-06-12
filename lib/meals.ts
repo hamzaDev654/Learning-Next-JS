c2bbfc61f37ebd3958738e369c7ee92731065fd9
hamzaDev654 <imhamza67@gmail.com>
2026-06-13 00:57:14 +0530

    testing with db

--- lib/meals.ts
+++ lib/meals.ts
@@ -1,4 +1,5 @@
 import fs from "node:fs";
+import path from "node:path";
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
@@ -14,17 +15,23 @@ export interface Meal {
   creator_email: string;
 }
 
-const db = sql("meals.db");
+const dbPath = path.join(process.cwd(), "meals.db");
+
+console.log("Database path:", dbPath);
+console.log("Database exists:", fs.existsSync(dbPath));
+
+const db = sql(dbPath);
 
 export async function getMeals(): Promise<Meal[]> {
   await new Promise((res) => setTimeout(res, 10000));
 
-  // throw new Error("loading meals fails");
   return db.prepare("SELECT * FROM meals").all() as Meal[];
 }
 
 export function getMeal(slug: string): Meal | undefined {
-  return db.prepare("SELECT * FROM meals WHERE slug =?").get(slug) as Meal | undefined;
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug) as
+    | Meal
+    | undefined;
 }
 
 export async function saveMeal(meal: Meal) {
@@ -34,30 +41,30 @@ export async function saveMeal(meal: Meal) {
   const extension = meal.image.name.split(".").pop();
   const fileName = `${meal.slug}.${extension}`;
 
-  const strem = fs.createWriteStream(`public/images/${fileName}`);
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
   const bufferImage = await meal.image.arrayBuffer();
 
-  strem.write(Buffer.from(bufferImage), (e: any) => {
+  stream.write(Buffer.from(bufferImage), (e: any) => {
     if (e) {
       throw new Error("Saving image failed!");
     }
   });
 
   meal.image = `/images/${fileName}`;
+
   db.prepare(
     `
     INSERT INTO meals
-    
     (title, summary, instructions, creator, creator_email, image, slug)
-    VALUES(
-        @title,
-        @summary,
-        @instructions,
-        @creator,
-        @creator_email,
-        @image,
-        @slug
-        )
-    `,
+    VALUES (
+      @title,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email,
+      @image,
+      @slug
+    )
+  `,
   ).run(meal);
 }
